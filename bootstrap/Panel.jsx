e14fc566d3dea846553b2ff6c650a1286a18067f
Andy Edwards <jedwards@fastmail.com>
2015-08-13 16:49:21 -0500

    whoops, panel collapse structure wasn't matching real Bootstrap

--- bootstrap/Panel.jsx
+++ bootstrap/Panel.jsx
@@ -10,7 +10,7 @@ Panel.Danger = addClass('div', 'panel panel-danger');
 Panel.Heading = addClass('div', 'panel-heading');
 Panel.Title = addClass('h3', 'panel-title');
 Panel.Body = addClass('div', 'panel-body');
-Panel.Body.Collapse = addClass(Collapse, 'panel-body panel-collapse');
+Panel.Collapse = addClass(Collapse, 'panel-collapse');
 Panel.Footer = addClass('div', 'panel-footer');
 
 export default Panel;
