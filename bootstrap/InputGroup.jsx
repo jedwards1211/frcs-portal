6db497afef7476c0c77a1a1d28a4d4816c3e5ee1
Andy Edwards <jedwards@fastmail.com>
2015-08-18 17:18:02 -0500

    created basic InputGroup components:

---
+++ bootstrap/InputGroup.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import addClass from '../wrappers/addClass';
+
+var InputGroup = addClass('div', 'input-group');
+InputGroup.Addon = addClass('div', 'input-group-addon');
+
+export default InputGroup;
