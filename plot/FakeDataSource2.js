cf869e61f17e16d4503724335d8b86eeca097a89
Andy Edwards <jedwards@fastmail.com>
2015-09-03 14:54:53 -0500

    make FakeDataSource2 a bit more efficient

--- plot/FakeDataSource2.js
+++ plot/FakeDataSource2.js
@@ -14,19 +14,57 @@ function stretchRandom(seed, time, period, amplitude) {
     return ((1 - f) * m0 + f * m1 - 0.5) * amplitude;
 }
 
+class NoiseGen {
+  constructor(seed, period, amplitude) {
+    this.seed = seed;
+    this.freq = 1 / period;
+    this.amplitude = amplitude;
+    this.cache = [];
+  }
+
+  cachedValue(time) {
+    if (!this.cache[time]) {
+      Math.seedrandom(time + this.seed);
+      this.cache[time] = Math.random();
+    }
+    return this.cache[time];
+  }
+
+  valueAt(time) {
+    var m = time * this.freq;
+    var m0 = Math.floor(m);
+    var m1 = m0 + 1;
+    var f = m - m0;
+    m0 = this.cachedValue(m0);
+    m1 = this.cachedValue(m1);
+    return ((1 - f) * m0 + f * m1 - 0.5) * this.amplitude;
+  }
+}
+
 export default class FakeDataSource {
   constructor(options = {}) {
-    this.increment            = options.increment           || 30000;
+    this.increment            = options.increment           || 10000;
     this.majorPeriod          = options.majorPeriod         || 3600000;
     this.majorAmplitude       = options.majorAmplitude      || 5;
     this.variancePeriod       = options.variancePeriod      || 4782052;
     this.varianceAmplitude    = options.varianceAmplitude   || 2;
     this.delay                = options.delay               || 1000;
+
+    this.majorGens = {};
+    this.varianceGens = {};
   }
 
   valueAt(channelId, time) {
-    var m = stretchRandom(channelId, time, this.majorPeriod, this.majorAmplitude);
-    var v = stretchRandom(channelId, time, this.variancePeriod, this.varianceAmplitude) + this.varianceAmplitude;
+    var majorGen = this.majorGens[channelId];
+    if (!majorGen) {
+      majorGen = this.majorGens[channelId] = new NoiseGen(channelId, this.majorPeriod, this.majorAmplitude);
+    }
+    var varianceGen = this.varianceGens[channelId];
+    if (!varianceGen) {
+      varianceGen = this.varianceGens[channelId] = new NoiseGen(channelId, this.variancePeriod, this.varianceAmplitude);
+    }
+    var m = majorGen.valueAt(time);
+    var v = varianceGen.valueAt(time) + this.varianceAmplitude;
     Math.seedrandom(time + channelId);
     return m + v * (Math.random() - 0.5);
   }
