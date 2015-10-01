9f41ba0aa27cabeebd2c831a7d01982d0907f669
Andy Edwards <jedwards@fastmail.com>
2015-10-01 11:10:58 -0500

    make BlockHandle use pageX/pageY if available

--- block-grid/BlockHandle.jsx
+++ block-grid/BlockHandle.jsx
@@ -3,6 +3,13 @@ import React, {Component} from 'react';
 export const horizontalDrag = ({x}) => ({x, y: 0});
 export const verticalDrag = ({y}) => ({x: 0, y});
 
+function getPosition(e) {
+  if ('pageX' in e) {
+    return {x: e.pageX, y: e.pageY};
+  }
+  return {x: e.clientX, y: e.clientY};
+}
+
 export default class BlockHandle extends Component {
   static propTypes = {
     disabled:          React.PropTypes.bool,
@@ -24,13 +31,13 @@ export default class BlockHandle extends Component {
       document.addEventListener('mouseup'  , this.onMouseUp);
 
       let {blockKey} = this.context;
-      this.onStart(blockKey, this.props.transformPosition({x: e.clientX, y: e.clientY}));
+      this.onStart(blockKey, this.props.transformPosition(getPosition(e)));
     }
   }
   onMouseMove = e => {
     e.preventDefault();
     let {blockKey} = this.context;
-    this.onMove(blockKey, this.props.transformPosition({x: e.clientX, y: e.clientY}));
+    this.onMove(blockKey, this.props.transformPosition(getPosition(e)));
   }
   onMouseUp = e => {
     e.preventDefault();
