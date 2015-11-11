893f795fa72c67d9ef6b985f295d9fb86fcc5004
Andy Edwards <andy@mindfront.com>
2015-11-11 05:45:45 -0600

    only call modifyDataSub when range/channels have changed

--- plot/DataCache.js
+++ plot/DataCache.js
@@ -346,17 +346,22 @@ export default class DataCache extends EventEmitter {
   }
 
   resubscribe = _.throttle((channelIds, beginTime, endTime) => {
-    if (this._unsubscribe) {
-      if (this.dataSource.modifySubscription) {
-        this.dataSource.modifySubscription({channelIds, beginTime, endTime});
+    let newSelector = {channelIds, beginTime, endTime};
+    if (!_.isEqual(this._selector, newSelector)) {
+      this._selector = newSelector;
+
+      if (this._unsubscribe) {
+        if (this.dataSource.modifySubscription) {
+          this.dataSource.modifySubscription({channelIds, beginTime, endTime});
+        }
+        else {
+          this._unsubscribe();
+        }
       }
       else {
-        this._unsubscribe();
+        let {onData} = this;
+        this._unsubscribe = this.dataSource.subscribe({channelIds, beginTime, endTime}, {onData});
       }
-    }
-    else {
-      let {onData} = this;
-      this._unsubscribe = this.dataSource.subscribe({channelIds, beginTime, endTime}, {onData});
     }
   }, 1000);
 
