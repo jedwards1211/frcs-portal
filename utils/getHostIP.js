4773341d6998d284708cb46612b47456148c2dae
Andy Edwards <andy@mindfront.com>
2015-12-03 21:42:34 -0600

    create getHostIP

---
+++ utils/getHostIP.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+var defaultInterfaces = ['en1', 'en0', 'wlan0', 'eth0', 'eth1'];
+
+function getHostIP(interfaces) {
+  interfaces = interfaces || defaultInterfaces;
+
+  for (var i = 0; i < interfaces.length; i++) {
+    var ifname = interfaces[i];
+    var iface = ifaces[ifname];
+    if (iface) {
+      for (var k = 0; k < iface.length; k++) {
+        if (iface[k].family === 'IPv4') {
+          return iface[k].address;
+        }
+      }
+    }
+  }
+}
+
+module.exports = getHostIP;
+module.exports.defaultInterfaces = defaultInterfaces;
+
+if (!module.parent) {
+  console.log(getHostIP());
+}
