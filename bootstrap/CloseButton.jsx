516b97cc4ea38427c55bfe68b3328571bc70be3b
Andy Edwards <andy@mindfront.com>
2015-12-08 15:06:20 -0600

    make CloseButton disable properly

--- bootstrap/CloseButton.jsx
+++ bootstrap/CloseButton.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default (props) => (<button {...props} type="button" className="close" aria-label="Close">
+import './CloseButton.sass';
+
+export default (props) => (<button {...props}
+  onClick={props.disabled ? undefined : props.onClick}
+  type="button" className="close" aria-label="Close">
   <span aria-hidden="true">&times;</span>
 </button>);
