abadb7b4a5580a195082f35e56009c7042da0181
Andy Edwards <andy@mindfront.com>
2015-12-10 15:05:34 -0600

    modernize and improve PageSlider (it smoothly adjusts its height now)

--- common/PageSlider.jsx
+++ common/PageSlider.jsx
@@ -1,124 +1,138 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 import {shouldComponentUpdate as shouldPureComponentUpdate} from 'react-addons-pure-render-mixin';
 import _ from 'lodash';
 
-require('./PageSlider.sass');
+import propAssign from '../utils/propAssign';
+import setStateChain from '../utils/setStateChain';
+import {getTimeout} from '../transition/callOnTransitionEnd';
+
+import './PageSlider.sass';
 
 /**
  * A simple page slider.  All of the children you provide to it are laid out horizontally,
  * each wrapped in a div with the same width as the PageSlider, and whenever you change
  * props.activeIndex, it translates with animation to the child at that index.
  */
-export default React.createClass({
-  propTypes: {
-    activeIndex:            React.PropTypes.number.isRequired,
-    transitionDuration:     React.PropTypes.number,
-    restrictMaxHeight:      React.PropTypes.bool,
-    useAbsolutePositioning: React.PropTypes.bool,
-    onTransitionEnd:        React.PropTypes.func,
-  },
-  getDefaultProps() {
-    return {
-      transitionDuration: 200,
-      restrictMaxHeight: false,
-    };
-  },
-  getInitialState() {
-    return {
+export default class PageSlider extends Component {
+  shouldComponentUpdate = shouldPureComponentUpdate;
+  static propTypes = {
+    activeIndex:            PropTypes.number.isRequired,
+    transitionDuration:     PropTypes.number,
+    restrictMaxHeight:      PropTypes.bool,
+    useAbsolutePositioning: PropTypes.bool,
+    onTransitionEnd:        PropTypes.func,
+    transitionHeight:       PropTypes.bool,
+  }
+  static defaultProps = {
+    onTransitionEnd() {},
+    transitionDuration: 200,
+    transitionHeight: true,
+    restrictMaxHeight: false,
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: props.activeIndex,
       transitioning: false
     };
-  },
+  }
   componentWillReceiveProps(nextProps) {
     if (this.props.activeIndex !== nextProps.activeIndex) {
-      clearTimeout(this.timeout);
-      this.setState({transitioning: true});
-      this.timeout = setTimeout(() => this.setState({transitioning: false}), nextProps.transitionDuration);
+      this.doTransition(nextProps.activeIndex);
     }
-  },
+  }
   componentWillMount() {
+    this._mounted = true;
     this._wrappers = [];
     this._childRefs = [];
-  },
+  }
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+  isMounted() {
+    return this._mounted;
+  }
   componentDidMount() {
     let child = this._childRefs[this.props.activeIndex];
     if (child && child.componentDidAppear) child.componentDidAppear();
-    this.componentDidUpdate();
-  },
-  componentWillUpdate(nextProps, nextState) {
-    this.wasTransitioning = this.state.transitioning;
-  },
-  componentDidUpdate() {
-    if (this.isMounted()) {
-      let {activeIndex} = this.props;
-      var activeElement = ReactDOM.findDOMNode(this._wrappers[activeIndex]);
-      if (activeElement && this.state.height !== activeElement.scrollHeight) {
-        this.setState({height: activeElement.scrollHeight});
-      }
-      if (this.wasTransitioning && !this.state.transitioning) {
-        let child = this._childRefs[activeIndex];
-        if (child && child.componentDidEnter) child.componentDidEnter();
-        if (this.props.onTransitionEnd) this.props.onTransitionEnd();
-      }
-    }
-  },
-  wrapChild(child, index) {
+  }
+  doTransition = (nextActiveIndex = this.props.activeIndex) => {
+    let {state: {activeIndex, transitioning}, props: {transitionHeight}} = this;
+
+    if (nextActiveIndex === activeIndex || transitioning) return false;
+
+    let sequence = _.compact([
+      transitionHeight && (cb => ({height: this._viewport.scrollHeight})),
+      cb => ({transitioning: true}),
+      cb => {
+        let nextState = {activeIndex: nextActiveIndex};
+        let wrapper = this._wrappers[nextActiveIndex];
+        if (transitionHeight && wrapper) {
+          nextState.height = wrapper.scrollHeight;
+        }
+        return nextState;
+      },
+      cb => setTimeout(cb, Math.max(getTimeout(this._viewport) || 0, getTimeout(this._root) || 0)),
+      cb => ({transitioning: false}),
+      transitionHeight && (cb => ({height: undefined})),
+      cb => {
+        if (!this.doTransition()) {
+          let child = this._childRefs[nextActiveIndex];
+          if (child && child.componentDidEnter) child.componentDidEnter();
+          this.props.onTransitionEnd();
+        }
+      },
+    ]);
+
+    setStateChain(this, ...sequence);
+    return true;
+  }
+  wrapChild = (child, index) => {
     if (!child) return child;
-    let {props: {activeIndex}, state: {transitioning}} = this;
+    let {state: {transitioning, activeIndex}, props: {transitionHeight}} = this;
 
     var style = child.props.style;
     if (this.props.useAbsolutePositioning) {
-      style = _.assign({}, style, {left: (index * 100) + '%'});
+      style = propAssign(style, {left: (index * 100) + '%'});
     }
 
     if (!transitioning && index !== activeIndex) {
-      style = _.assign({}, style, {visibility: 'hidden'});
+      style = propAssign(style, {visibility: 'hidden'});
+      if (transitionHeight) {
+        style.height = 0;
+      }
     }
 
     return <div key={index} ref={c => this._wrappers[index] = c} style={style}>
       {React.cloneElement(child, {ref: c => this._childRefs[index] = c})}
     </div>;
-  },
-  shouldComponentUpdate: shouldPureComponentUpdate,
+  }
   render() {
-    var {activeIndex, transitionDuration, restrictMaxHeight, className, style} = this.props;
-    var {height} = this.state;
+    var {className, style} = this.props;
+    var {height, transitioning, activeIndex} = this.state;
 
     var transform = 'translateX(' + (-activeIndex * 100) + '%)';
 
-    var transition = 'all ease-out ' + (transitionDuration / 1000) + 's';
+    className = classNames(className, 'mf-page-slider', {transitioning});
 
-    if (restrictMaxHeight) {
-      style = _.assign({}, style, {
-        maxHeight: height,
-      });
-    }
+    style = propAssign(style, {height});
 
-    if (className) className += ' mf-page-slider';
-    else className = 'mf-page-slider';
-
-    return (
-      <div ref="pageSlider" {...this.props} className={className} style={style}>
-        <div 
-          className="viewport" 
-          ref="viewport" 
-          style={{
-            'WebkitTransform': transform,
-            'KhtmlTransform': transform,
-            'MozTransform': transform,
-            'msTransform': transform,
-            'OTransform': transform,
-            'transform': transform,
-            'WebkitTransition': transition,
-            'KhtmlTransition': transition,
-            'MozTransition': transition,
-            'msTransition': transition,
-            'OTransition': transition,
-            'transition': transition,
-          }}>
-          {React.Children.map(this.props.children, this.wrapChild)}
-        </div>
+    return <div {...this.props} ref={c => this._root = c} className={className} style={style}>
+      <div 
+        className="viewport" 
+        ref={c => this._viewport = c}
+        style={{
+          'WebkitTransform': transform,
+          'KhtmlTransform': transform,
+          'MozTransform': transform,
+          'msTransform': transform,
+          'OTransform': transform,
+          'transform': transform,
+        }}>
+        {React.Children.map(this.props.children, this.wrapChild)}
       </div>
-    );
+    </div>;
   }
-});
+}
--- utils/setStateChain.js
+++ utils/setStateChain.js
@@ -1,3 +1,12 @@
+/**
+ * Calls zero or more callbacks in sequence.  If a callback returns an object, the component's
+ * state is updated with that object (for the sake of shortening transition sequence code).
+ * 
+ * @param component - a React component
+ * @param {function[]} callbacks - a series of callbacks taking the next callback
+ *                                 as argument.  If a callback returns an object,
+ *                                 this will call component.setState(<object>, <next callback>).
+ */
 export default function setStateChain(component, ...callbacks) {
   callbacks.reduceRight(
     (cb, fn) => () => setTimeout(
