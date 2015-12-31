64e60cbbe581a8d3b564f08f76e20208e6860511
Andy Edwards <andy@mindfront.com>
2015-12-31 11:16:12 -0600

    add parseNumber method

--- utils/validationRegExps.js
+++ utils/validationRegExps.js
@@ -13,3 +13,18 @@ export const unsignedIntegerOrBlankRegExp = /^\s*\d*\s*$/;
 export function isEmptyValue(value) {
   return value === '' || value === undefined || value === null;
 }
+
+/**
+ * @param {number|string} value the number or string to parse.
+ * @param {regExp} the regular expression specifying allowed string values.
+ * @returns value if it is a number, the number parsed from it if it was
+ * a string matching regExp, or undefined.
+ */
+export function parseNumber(value, regExp) {
+  if (typeof value === 'string' && regExp.test(value)) {
+    return parseFloat(value);
+  }
+  else if (typeof value === 'number' && !isNaN(value)) {
+    return value;
+  }
+}
