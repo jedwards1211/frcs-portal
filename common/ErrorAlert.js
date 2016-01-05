2c592b10f7f8d31f3e4d903e2f1fc0fd2337e251
Andy Edwards <andy@mindfront.com>
2016-01-05 12:04:44 -0600

    fix error messages in TypicalModal

---
+++ common/ErrorAlert.js
@@ -0,0 +1,30 @@
+import React, {Component, PropTypes} from 'react';
+
+import Alert from '../bootstrap/Alert';
+
+export function errorMessage(err) {
+  if (err instanceof Error) {
+    return err.message || err.toString();
+  }
+  if (typeof err !== 'string' || !err) {
+    return 'an unknown error has occurred';
+  }
+  return err;
+}
+
+export default class ErrorAlert extends Component {
+  static propTypes = {
+    error: PropTypes.oneOfType([
+      PropTypes.instanceof(Error),
+      PropTypes.string,
+      PropTypes.node,
+    ]).isRequired,
+  }
+  render() {
+    const {error} = this.props;
+    if (React.isValidElement(error)) {
+      return error;
+    }
+    return <Alert.Danger>{errorMessage(error)}</Alert.Danger>;
+  }
+}
--- common/TypicalModal.jsx
+++ common/TypicalModal.jsx
@@ -6,9 +6,9 @@ import CollapseTransitionGroup from '../transition/CollapseTransitionGroup';
 
 import Modal from '../bootstrap/Modal';
 import CloseButton from '../bootstrap/CloseButton';
-import Alert from '../bootstrap/Alert';
 import Button from '../bootstrap/Button';
 import Spinner from '../common/Spinner';
+import ErrorAlert from '../common/ErrorAlert';
 
 import './TypicalModal.sass';
 
@@ -51,14 +51,11 @@ export default class TypicalModal extends React.Component {
         saving, error, errors, className, children} = this.props;
 
     if (error) {
-      errors.__singleError = error;
+      errors = Object.assign({}, errors, {__singleError: error});
     }
 
     let errorAlerts = <CollapseTransitionGroup component="div" collapseProps={{className: 'error-collapse'}}>
-      {_.map(errors, (error, key) => {
-        if (React.isValidElement(error)) return error;
-        else if (error) return <Alert.Danger key={key}>{error && error.toString()}</Alert.Danger>;
-      })}
+      {_.map(errors, (error, key) => (<ErrorAlert key={key} error={error}/>))}
     </CollapseTransitionGroup>;
 
     className = classNames(className, 'mf-typical-modal');
