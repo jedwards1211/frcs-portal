77a8c0c757571c377842ab73adbcc228f31e8f41
Andy Edwards <andy@mindfront.com>
2016-01-06 16:48:49 -0600

    create Popover and PopoverController

---
+++ bootstrap/Popover.jsx
@@ -0,0 +1,25 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+
+export default class Popover extends Component {
+  static propTypes = {
+    title: PropTypes.node,
+    side:  PropTypes.oneOf(['top', 'left', 'bottom', 'right'])
+  }
+  static defaultProps = {
+    side: 'top'
+  }
+  render() {
+    let {className, side, title, children} = this.props;
+
+    className = classNames(className, 'popover', side);
+
+    return <div {...this.props} className={className}>
+      <div className="arrow"/>
+      <h3 className="popover-title">{title}</h3>
+      <div className="popover-content">
+        {children}
+      </div>
+    </div>;
+  }
+}
