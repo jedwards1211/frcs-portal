4306ec6ab52493a5181e62dfcdd1e1b12e1acca6
Andy Edwards <andy@mindfront.com>
2016-01-13 21:21:27 -0600

    add a lot of comments to AutoFatTracePlotter

--- plot/AutoFatTracePlotter.js
+++ plot/AutoFatTracePlotter.js
@@ -1,8 +1,11 @@
-// tracks info about a group of points that are squashed together in a single
-// column (or row) of pixels
+// accumulates info about a group of points that are squashed together in a single
+// column (or row) of pixels, so that (basically) a single line or fill polygon can
+// be drawn to approximate those points
 class Column {
   // whether all points seen in the current column of pixels are
-  // monotonic (only increasing or decreasing from left to right)
+  // monotonic (values are strictly increasing or decreasing along domain axis)
+  // in other words, if monotonic is false, the trace should be fat
+  // within the column.
   monotonic = true
 
   // the first point in the current column
@@ -72,6 +75,10 @@ class Column {
  * column of pixels, a "fat trace" fill will be drawn into that column 
  * (and adjacent columns of the same type).
  *
+ * To use it, pass in points with increasing domain to addPoint(),
+ * and then call flush() after all points to plot have been added.
+ * Call reset() before plotting again with this instance.
+ *
  * Interfaces used:
  * 
  * domainConversion, valueConversion:
@@ -96,18 +103,18 @@ export default class AutoFatTracePlotter {
     // points on a line waiting to be flushed to the renderer
     this.linePoints = [];        
 
-    // points on the bottom side of the fill waiting to be
+    // points on the min side of the fill waiting to be
     // flushed to the renderer
     this.fillMinPoints = [];
 
-    // points on the top side of the fill waiting to be
+    // points on the max side of the fill waiting to be
     // flushed to the renderer
     this.fillMaxPoints = [];
 
-    // stats for the current column
+    // accumulator for the current column
     this.column = new Column();
 
-    // stats for the previous column
+    // accumulator for the previous column
     this.prevColumn = new Column();
   }
 
@@ -124,24 +131,25 @@ export default class AutoFatTracePlotter {
    * Adds a point to be plotted.
    * @param {domain} the domain of the point.
    * @param {value} the value of the point.
-   * @param {traceRenderer} a renderer interface (see notes at top)
-   * @param {domainConversion} conversion for domain axis (see notes at top)
-   * @param {valueConversion} conversion for value axis (see notes at top)
    */
   addPoint( domain , value ) {
     // coerce null, undefined, or other non-number types to NaN
     if (isNaN(value) || value === null) value = NaN;
-
+    
     var columnX = this.domainConversion.convert( this.column.start[0] );
     var viewX = this.domainConversion.convert( domain );
     
-    // if the added point is not in the same column, advance the column
-
+    // if the added point is not in the same column of pixels as the last, plot the data
+    // between the columns.  This way if there are a lot of points crammed into this small
+    // space they can be plotted with (basically) one fill (fat trace) or line (if the points
+    // are monotonic)
     if( !isNaN( this.column.start[0] ) && Math.round( viewX ) > Math.round( columnX ) )
     {
       this._advanceColumn( false );
     }
 
+    // accumulate information about what to plot until a call to _advanceColumn plots
+    // and clears it.
     this.column.addPoint(domain, value);
   }
     
@@ -153,47 +161,67 @@ export default class AutoFatTracePlotter {
     this._advanceColumn( true );
   }
 
+  /**
+   * Plots the data between the previous and current columns, and then begins a new column.
+   * This is where the magic happens, there's a lot of tedious logic to decide when to plot
+   * a fat trace and when to plot a line, and when to transition between them.
+   */
   _advanceColumn( forceFlush )
   {
     let {prevColumn, column} = this;
 
+    // are prevColumn and column adjacent columns of pixels?  If not, we should connect
+    // them with a line, even if the trace is fat in the columns themselves
     let nonadjacent = Math.round(this.domainConversion.convert(column.start[0])) >
       Math.round(this.domainConversion.convert(prevColumn.start[0])) + 1;
 
     let push = Array.prototype.push;
 
     if (isNaN(prevColumn.end[1]) || isNaN(column.start[1])) {
+      // data is discontiguous; prevColumn and column shouldn't be joined by
+      // line or fill
       if (!isNaN(prevColumn.end[1])) {
         if (!prevColumn.monotonic) {
+          // collapse from fill to a line
           push.apply(this.fillMinPoints, prevColumn.end);
           push.apply(this.fillMaxPoints, prevColumn.end);
           push.apply(this.linePoints, prevColumn.end);
         }
+        // draw horizontal line from last defined point to time of NaN value
         this.linePoints.push(column.start[0], prevColumn.end[1]);
       }
+      // end previous trace segment...
       this._flushLine();
       this._flushFill();
+      // ...so that next trace segment isn't connected
       if (!isNaN(column.min[0])) {
         if (column.monotonic) {
           push.apply(this.linePoints, column.validStart);
           push.apply(this.linePoints, column.validEnd);
         }
         else { // !column.monotonic
+          // expand from line...
           push.apply(this.fillMinPoints, column.validStart);
-          push.apply(this.fillMaxPoints, column.validEnd);
+          push.apply(this.fillMaxPoints, column.validStart);
+          // ...to fill
           push.apply(this.fillMinPoints, column.min);
           push.apply(this.fillMaxPoints, column.max);
         }
       }
     }
     else { // !isNaN(prevColumn.end[1]) && !isNaN(column.start[1])
+      // data is contiguous; prevColumn and column should be joined by
+      // a line or fill
       if (prevColumn.monotonic) {
         if (column.monotonic) {
+          // extend line
           push.apply(this.linePoints, column.start);
           push.apply(this.linePoints, column.end);
         }
         else { // !column.monotonic
+          // expand from line...
           if (nonadjacent) {
+            // continue line up to this column before expanding to fill
             push.apply(this.linePoints, column.start);
             this._flushLine();
             push.apply(this.fillMinPoints, column.start);
@@ -204,12 +232,14 @@ export default class AutoFatTracePlotter {
             push.apply(this.fillMinPoints, prevColumn.end);
             push.apply(this.fillMaxPoints, prevColumn.end);
           }
+          // ...to fill
           push.apply(this.fillMinPoints, column.min);
           push.apply(this.fillMaxPoints, column.max);
         }
       }
       else { // !prevColumn.monotonic
         if (column.monotonic) {
+          // collapse from fill...
           if (nonadjacent) {
             push.apply(this.fillMinPoints, prevColumn.end);
             push.apply(this.fillMaxPoints, prevColumn.end);
@@ -221,20 +251,26 @@ export default class AutoFatTracePlotter {
             push.apply(this.fillMaxPoints, column.start);
             this._flushFill();
           }
+          // ...to line
           push.apply(this.linePoints, column.start);
           push.apply(this.linePoints, column.end);
         }
         else { // !column.monotonic
           if (nonadjacent) {
+            // trace is filled (fat) in both columns but they're not adjacent,
+            // so collapse from fill...
             push.apply(this.fillMinPoints, prevColumn.end);
             push.apply(this.fillMaxPoints, prevColumn.end);
             this._flushFill();
+            // ...down to a line between the columns...
             push.apply(this.linePoints, prevColumn.end);
             push.apply(this.linePoints, column.start);
             this._flushLine();
+            // ...then expand back to a fill
             push.apply(this.fillMinPoints, column.start);
             push.apply(this.fillMaxPoints, column.start);
           }
+          // extend filled region
           push.apply(this.fillMinPoints, column.min);
           push.apply(this.fillMaxPoints, column.max);
         }
@@ -246,12 +282,17 @@ export default class AutoFatTracePlotter {
       this._flushFill();
     }
 
+    // begin a new column
     let swap = this.column;
     this.column = this.prevColumn;
     this.prevColumn = swap;
     this.column.clear();
   }
 
+  /**
+   * Copies an array of alternating domain/value pairs and converts
+   * them from raw data to output coordinates.
+   */
   _copyAndConvert(points) {
     let {domainConversion, valueConversion} = this;
     let result = [];
@@ -262,6 +303,9 @@ export default class AutoFatTracePlotter {
     return result;
   }
   
+  /**
+   * Sends the accumulated line data to the TraceRenderer and clears it.
+   */
   _flushLine( )
   {
     if( this.linePoints.length > 1 )
@@ -271,6 +315,9 @@ export default class AutoFatTracePlotter {
     this.linePoints.length = 0;
   }
   
+  /**
+   * Sends the accumulated fill data to the TraceRenderer and clears it.
+   */
   _flushFill( )
   {
     if( this.fillMinPoints.length > 1 && this.fillMaxPoints.length > 1 )
