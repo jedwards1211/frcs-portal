6e8f4c5b431d4e3081ca8c28ef8088379b923778
Andy Edwards <jedwards@fastmail.com>
2016-02-02 16:41:36 -0600

    fix potential NPE

--- wrappers/addClass.jsx
+++ wrappers/addClass.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 
 export default function addClass(Component, addedClassName) {
-  return (props) => (<Component {...props} className={classNames(props.className, addedClassName)}>
-    {props.children}
-  </Component>);
+  return (props) => {
+    if (props) {
+      return <Component {...props} className={classNames(props.className, addedClassName)}>
+        {props.children}
+      </Component>;
+    }
+    else {
+      return <Component className={addedClassName}/>;
+    }
+  };
 }
