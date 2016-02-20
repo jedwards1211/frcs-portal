2374e2ab14e232f42aadc6c35a3b7c3b95742748
Andy Edwards <jedwards@fastmail.com>
2016-02-19 18:35:41 -0600

    a few Glyphicon tweaks

--- bootstrap/Glyphicon.jsx
+++ bootstrap/Glyphicon.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import _ from 'lodash';
 
+import propAssign from '../utils/propAssign';
+
+const EXCLUDED_PROPS = [
+  'className',
+  'style',
+  'float',
+  'onClick',
+];
+
 export default props => {
-  let className = classNames(props.className, 'glyphicon', _.map(props,
-    (val, prop) => prop && 'glyphicon-' + _.kebabCase(prop)));
-  return <i className={className}/>;
+  let otherProps = _.pick(props, EXCLUDED_PROPS);
+  let className = classNames(props.className, 'glyphicon',
+    _.mapKeys(_.omit(props, EXCLUDED_PROPS), (val, prop) => prop && 'glyphicon-' + _.kebabCase(prop)));
+  otherProps.style = propAssign(otherProps.style, {float: props.float});
+  return <i {...otherProps} className={className}/>;
 };
