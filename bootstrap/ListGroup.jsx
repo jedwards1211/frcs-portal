e769c16d841a014322c17bfab15726030cbd2ceb
Andy Edwards <jedwards@fastmail.com>
2016-02-25 11:12:09 -0600

    finish refining ListGroup

--- bootstrap/ListGroup.jsx
+++ bootstrap/ListGroup.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import Badge from './Badge.jsx';
 import {getContextClass} from './bootstrapPropUtils';
 
-import {Title, Body} from '../common/View.jsx';
+import {Header, Title, Body} from '../common/View.jsx';
 
 type DefaultProps = {
   component: ReactTag
@@ -18,6 +18,27 @@ type Props = {
   children?: any,
 };
 
+/**
+ * A super-convenient way to create Bootstrap list groups.  Children can be any type of element you want,
+ * and you can give them the following additional props (the children will be cloned with additional classNames
+ * and children based upon what props you specify):
+ *
+ * * contextClass - bootstrap context class (danger/alarm/error/warning/success/ok/info)
+ * * danger(/alarm/error) - use .list-group-item-danger context class
+ * * warning - use .list-group-item-warning context class
+ * * success(/ok) - use .list-group-item-success context class
+ * * info - use .list-group-item-info context class
+ * * active - use .active class
+ * * disabled - use .disabled class
+ * * badge - add a badge to the list item
+ * * header - add a <Header> (will get .list-group-item-heading class)
+ * * title - add a <Title> (inside a default <Header> if not given)
+ *
+ * In addition, you may use the usual View, Header, Title, and Body skinnable components inside children,
+ * and they will be rendered using the correct bootstrap classes.
+ *
+ * The root component will default to 'div'.  You may override it by passing a `component` prop.
+ */
 export default class ListGroup extends Component<DefaultProps,Props,void> {
   static defaultProps = {
     component: 'div'
@@ -25,12 +46,16 @@ export default class ListGroup extends Component<DefaultProps,Props,void> {
   static childContextTypes = {
     TitleSkin: PropTypes.any.isRequired,
     TitleClassName: PropTypes.string.isRequired,
+    HeaderSkin: PropTypes.any.isRequired,
+    HeaderClassName: PropTypes.string.isRequired,
     BodyClassName: PropTypes.string.isRequired,
   };
   getChildContext(): Object {
     return {
-      TitleSkin: 'h4',
-      TitleClassName: "list-group-item-heading",
+      TitleSkin: 'div',
+      TitleClassName: "list-group-item-title",
+      HeaderSkin: 'h4',
+      HeaderClassName: "list-group-item-heading",
       BodyClassName: "list-group-item-text"
     };
   }
@@ -38,15 +63,32 @@ export default class ListGroup extends Component<DefaultProps,Props,void> {
     if (!child) {
       return child;
     }
-    let {className, children, title, active, disabled, badge} = child.props;
+    let {className, children, header, title, active, disabled, badge} = child.props;
 
     let contextClass = getContextClass(child.props);
     className = classNames(className, 'list-group-item',
       contextClass && 'list-group-item-' + contextClass, {active, disabled});
 
     if (title) {
+      if (header instanceof Array) {
+        header = [
+          <Title key="title">{title}</Title>,
+          ...header
+        ];
+      }
+      else if (header) {
+        header = [
+          <Title key="title">{title}</Title>,
+          header
+        ];
+      }
+      else {
+        header = <Title>{title}</Title>;
+      }
+    }
+    if (header) {
       children = [
-        <Title key="title">{title}</Title>,
+        <Header key="header">{header}</Header>,
         <Body key="body">{children}</Body>
       ];
     }
