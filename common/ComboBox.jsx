26d13e9a317b0aea979457d770463711a432b48f
Andy Edwards <jedwards@fastmail.com>
2016-02-27 00:06:44 -0600

    code cleanup

--- common/ComboBox.jsx
+++ common/ComboBox.jsx
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
 
 import Dropdown from '../bootstrap/Dropdown';
+import Button from '../bootstrap/Button';
 
 import './ComboBox.sass';
 
@@ -34,23 +35,21 @@ export default class ComboBox extends Component {
     let hasItems = items && items.length;
 
     className = classNames(className, 'mf-combo-box', {'no-selection': noSelection});
-    toggleButtonClassName = classNames(toggleButtonClassName, 'btn btn-default');
 
     return <Dropdown {...this.props} className={className}>
-      <Dropdown.Toggle component="button" type="button" className={toggleButtonClassName}
-        disabled={disabled || !hasItems}>
+      <Button caret className={toggleButtonClassName} disabled={disabled || !hasItems}>
         <span ref={c => this.selectedItem = c} className="selected-item">
           {renderSelectedItem(noSelection ? 
             (hasItems ? placeholder : noItemsPlaceholder)
             : 
             selectedItem)}
-        </span> <span className="caret"/>
-      </Dropdown.Toggle>
-      {hasItems && <Dropdown.Menu component="ul" ref={c => this.dropdownMenu = c}>
+        </span>
+      </Button>
+      {hasItems && <ul ref={c => this.dropdownMenu = c}>
         {items.map((item, index) => (<li key={index} onClick={() => onChange(item, index)}>
           <a>{renderItem(item)}</a>
         </li>))}
-      </Dropdown.Menu>}
+      </ul>}
     </Dropdown>;
   }
 }
