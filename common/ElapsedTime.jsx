f9c5a3114e25c7759eb8aa7448c3baf3e07fb7f7
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2016-03-06 18:34:04 -0600

    Made the formatting logic of ElapsedTime available in a string, in addition to a react component.

--- common/ElapsedTime.jsx
+++ common/ElapsedTime.jsx
@@ -12,18 +12,26 @@ type Props = {
 };
 
 const ElapsedTime: (props: Props) => ReactElement = (props) => {
-  let {millis, showMillis} = props;
-  if (!showMillis) {
-    // round
-    millis = modFloor(millis + 500, 1000);
-  }
-  let hours   = millis > 3600000 && (Math.floor(millis / 3600000) + ':')
-  let minutes = _.padStart(Math.floor(millis / 60000) % 60, 2, '0');
-  let seconds = _.padStart(Math.floor(millis /  1000) % 60, 2, '0');
-  let milliseconds = showMillis && (':' + _.padStart(Math.floor(millis % 1000), 3, '0'));
+  const {millis, showMillis} = props;
   return <div {...props} style={Object.assign({textAlign: 'right'}, props.style)}>
-    {hours}{minutes}:{seconds}{milliseconds}
+    {formatElapsedTime(millis, { showMillis })}
   </div>;
 };
 
 export default ElapsedTime;
+
+type ElapsedTimeOpts = {
+  showMillis?: boolean
+}
+
+export function formatElapsedTime(millis: number, options?: ElapsedTimeOpts = {}) : string {
+  if (!options.showMillis) {
+    // round
+    millis = modFloor(millis + 500, 1000);
+  }
+  const hours   = (millis > 3600000) ? (Math.floor(millis / 3600000) + ':') : '';
+  const minutes = _.padStart(Math.floor(millis / 60000) % 60, 2, '0');
+  const seconds = _.padStart(Math.floor(millis /  1000) % 60, 2, '0');
+  const milliseconds = options.showMillis ? (':' + _.padStart(Math.floor(millis % 1000), 3, '0')) : '';
+  return `${hours}${minutes}:${seconds}${milliseconds}`;
+}
