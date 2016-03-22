3c68ea854702e78c0ce53e1e93a3b3ef78591c6b
Andy Edwards <jedwards@fastmail.com>
2016-03-21 22:02:08 -0500

    create requireUserRoles function

---
+++ meteor/requireUserRoles.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import _ from 'lodash';
+
+export default function requireUserRoles(userId: string, roles: string[]) {
+  let user = Meteor.users.find({_id: userId}, {roles: 1}).fetch();
+  if (!user) {
+    throw new Meteor.Error(401, 'Not authorized');
+  }
+  if (_.difference(roles, user.roles || []).length) {
+    throw new Meteor.Error(403, 'Forbidden');
+  }
+}
