c9d0b691c35fb12ca91562e879f5aa4688fed496
Andy Edwards <jedwards@fastmail.com>
2016-03-24 17:09:52 -0500

    create someDeep function

---
+++ utils/someDeep.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import _ from 'lodash';
+
+export default function someDeep(collection: Array<any> | Object, 
+  predicate?: (value: any, key: number | string, collection: Array<any> | Object) => any = _.identity): boolean {
+  return _.some(collection, (value, key, collection) => predicate(value, key, collection) || 
+    (value instanceof Object && someDeep(value, predicate)));
+}
