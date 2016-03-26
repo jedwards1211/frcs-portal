651e4250301902cf3912f5dec9e59bee0522b5c1
Andy Edwards <jedwards@fastmail.com>
2016-03-26 13:43:13 -0500

    create SortTh

---
+++ common/SortTh.jsx
@@ -0,0 +1,65 @@
+/* @flow */
+
+import React from 'react';
+import Glyphicon from '../bootstrap/Glyphicon.jsx';
+import _ from 'lodash';
+
+type OrderBy = {[field: string]: 'asc' | 'desc'};
+
+type Props = {
+  field: string,
+  orderBy?: OrderBy,
+  orderByMaxSize?: number,
+  onOrderByChange?: (orderBy: OrderBy) => any,
+  children: any
+};
+
+const SortTh: (props: Props) => ReactElement = props => {
+  let {field, children} = props;
+  let orderBy = props.orderBy || {};
+  
+  const onClick = () => {
+    let {onOrderByChange, orderByMaxSize} = props;
+    orderByMaxSize = orderByMaxSize || 1;
+    if (onOrderByChange) {
+      let oldOrderBy = props.orderBy || {};
+      let orderBy = {};
+      switch (oldOrderBy[field]) {
+        case 'asc':   orderBy[field] = 'desc'; break;
+        case 'desc':  break;
+        default:      orderBy[field] = 'asc'; break;
+      }
+      let size = 1;
+      for (let otherField in oldOrderBy) {
+        if (size >= orderByMaxSize) break;
+        if (otherField !== field) {
+          orderBy[otherField] = oldOrderBy[otherField];
+          size++;
+        }
+      }
+      onOrderByChange(orderBy);
+    }
+  };
+  
+  let sortIndex = 0;
+  for (let otherField in orderBy) {
+    if (otherField === field) break;
+    sortIndex++;
+  }
+  
+  let iconStyle = {
+    opacity: 1 - sortIndex / _.size(orderBy)
+  };
+  
+  let icon;
+  switch (orderBy[field]) {
+    case 'asc':   icon = <Glyphicon triangleBottom  style={iconStyle}/>; break;
+    case 'desc':  icon = <Glyphicon triangleTop     style={iconStyle}/>; break;
+  }
+  
+  return <th onClick={onClick}>
+    {icon} {children}
+  </th>;
+};
+
+export default SortTh;
