e7d59f696fedb7c4f80beba56d09ee3ae1beffa6
Andy Edwards <jedwards@fastmail.com>
2016-03-27 23:23:35 -0500

    fix requireUserRoles

--- meteor/requireUserRoles.js
+++ meteor/requireUserRoles.js
@@ -4,13 +4,7 @@ import _ from 'lodash';
 
 export default function requireUserRoles(userId: string, roles: string | string[]) {
   let normRoles = roles instanceof Array ? roles : [roles];
-  let user;
-  if (userId === Meteor.userId()) {
-    user = Meteor.user();
-  }  
-  else {
-    user = Meteor.users.find({_id: userId}, {roles: 1}).fetch();
-  }
+  let user = Meteor.users.findOne({_id: userId}, {roles: 1});
   if (!user) {
     throw new Meteor.Error(401, 'Not authorized');
   }
