ef6f58dab027cfe8c5c49f657bc5c798547a7736
Andy Edwards <jedwards@fastmail.com>
2016-03-29 09:12:02 -0500

    add more things to meteorTypes

--- flowtypes/meteorTypes.js
+++ flowtypes/meteorTypes.js
@@ -1,5 +1,50 @@
 /* @flow */
 
+export type QueryHandle = {
+  stop(): void
+};
+
+export type ObserveCallbacks = {
+  added?: (document: Object) => any,
+  addedAt?: (document: Object, atIndex: number, before: string) => any,
+  changed?: (newDocument: Object, oldDocument: Object) => any,
+  changedAt?: (newDocument: Object, oldDocument: Object, atIndex: number) => any,
+  removed?: (oldDocument: Object) => any,
+  removedAt?: (oldDocument: Object, atIndex: number) => any,
+  movedTo?: (document: Object, fromIndex: number, toIndex: number, before: string) => any
+};
+
+export type ObserveChangesCallbacks = {
+  added?: (id: string, fields: Object) => any,
+  addedBefore?: (id: string, fields: Object, before: string) => any,
+  changed?: (id: string, fields: Object) => any,
+  movedBefore?: (id: string, before: string) => any,
+  removed?: (id: string) => any
+};
+
+export type Transform = (document: Object) => Object;
+export type Selector = Object | Mongo.ObjectID | string;
+export type QueryOptions = {
+  sort?: {[field: string]: number} | Array<string | [string, 'asc' | 'desc']>,
+  skip?: number,
+  limit?: number,
+  fields?: {[field: string]: number},
+  reactive?: boolean,
+  transform?: Transform
+};
+export type UpdateOptions = {
+  multi?: boolean,
+  upsert?: boolean
+};
+
+export type AllowOptions = {
+  insert?: (userId: string, doc: Object) => ?boolean,
+  update?: (userId: string, doc: Object, fieldNames: string[], modifier: Object) => ?boolean,
+  remove?: (userId: string, doc: Object) => ?boolean,
+  fetch?: string[],
+  transform?: Transform
+};
+
 export type SubscriptionHandle = {
   stop(): void,
   ready(): boolean,
