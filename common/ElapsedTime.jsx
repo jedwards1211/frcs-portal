a5e3a9f343c05f2a7d775db7be5c5bd6622a4742
Andy Edwards <jedwards@fastmail.com>
2016-03-29 11:07:51 -0500

    extract formatElapedTime function

--- common/ElapsedTime.jsx
+++ common/ElapsedTime.jsx
@@ -2,9 +2,8 @@
 
 import React from 'react';
 import classNames from 'classnames';
-import _ from 'lodash';
 
-import {modFloor} from '../plot/GridMath';
+import formatElapsedTime from '../utils/formatElapsedTime';
 
 import './ElapsedTime.sass';
 
@@ -26,19 +25,3 @@ const ElapsedTime: (props: Props) => ReactElement = (props) => {
 };
 
 export default ElapsedTime;
-
-type ElapsedTimeOpts = {
-  showMillis?: boolean
-}
-
-export function formatElapsedTime(millis: number, options?: ElapsedTimeOpts = {}) : string {
-  if (!options.showMillis) {
-    // round
-    millis = modFloor(millis + 500, 1000);
-  }
-  const hours   = (millis > 3600000) ? (Math.floor(millis / 3600000) + ':') : '';
-  const minutes = _.padStart(Math.floor(millis / 60000) % 60, 2, '0');
-  const seconds = options.showSeconds ? (':' + _.padStart(Math.floor(millis /  1000) % 60, 2, '0')) : '';
-  const milliseconds = options.showMillis ? (':' + _.padStart(Math.floor(millis % 1000), 3, '0')) : '';
-  return `${hours}${minutes}${seconds}${milliseconds}`;
-}
---
+++ utils/formatElapsedTime.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+import _ from 'lodash';
+
+import {modFloor} from '../plot/GridMath';
+
+type ElapsedTimeOpts = {
+  showMillis?: boolean
+}
+
+export default function formatElapsedTime(millis: number, options?: ElapsedTimeOpts = {}) : string {
+  if (!options.showMillis) {
+    // round
+    millis = modFloor(millis + 500, 1000);
+  }
+  const hours   = (millis > 3600000) ? (Math.floor(millis / 3600000) + ':') : '';
+  const minutes = _.padStart(Math.floor(millis / 60000) % 60, 2, '0');
+  const seconds = options.showSeconds ? (':' + _.padStart(Math.floor(millis /  1000) % 60, 2, '0')) : '';
+  const milliseconds = options.showMillis ? (':' + _.padStart(Math.floor(millis % 1000), 3, '0')) : '';
+  return `${hours}${minutes}${seconds}${milliseconds}`;
+}
