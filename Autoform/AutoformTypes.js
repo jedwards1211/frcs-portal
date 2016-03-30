5a9cf43feaab6ec2348968829a209f3cca307a64
Andy Edwards <jedwards@fastmail.com>
2016-03-30 17:16:22 -0500

    delete Autoform

--- Autoform/AutoformTypes.js
+++
@@ -1,4 +0,0 @@
-/* @flow */
-
-export type AutoformFieldChangeCallback =
-  (autoformField: string, newValue: any, options?: {autoformPath?: Array<string | number>}) => any;
--- Autoform/autoformReducer.js
+++
@@ -1,7 +0,0 @@
-export default function autoformReducer(state, action) {
-  if (action.meta && action.meta.autoformField) {
-    let {payload, meta: {reduxPath, autoformPath, autoformField}} = action;
-    return state.setIn([...(reduxPath || []), ...(autoformPath || []), autoformField], payload);
-  }
-  return state;
-}
