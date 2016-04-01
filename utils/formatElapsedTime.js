107e20fca1a27b7efbcc60305926e26d4c67b3ea
Andy Edwards <jedwards@fastmail.com>
2016-03-31 22:29:15 -0500

    display 0 hours when showSeconds and showMillis are false

--- utils/formatElapsedTime.js
+++ utils/formatElapsedTime.js
@@ -13,7 +13,8 @@ export default function formatElapsedTime(millis: number, options?: ElapsedTimeO
     // round
     millis = modFloor(millis + 500, 1000);
   }
-  const hours   = (millis > 3600000) ? (Math.floor(millis / 3600000) + ':') : '';
+  const hours   = (millis > 3600000 || !(options.showSeconds || options.showMillis)) ? 
+    (Math.floor(millis / 3600000) + ':') : '';
   const minutes = _.padStart(Math.floor(millis / 60000) % 60, 2, '0');
   const seconds = options.showSeconds ? (':' + _.padStart(Math.floor(millis /  1000) % 60, 2, '0')) : '';
   const milliseconds = options.showMillis ? (':' + _.padStart(Math.floor(millis % 1000), 3, '0')) : '';
