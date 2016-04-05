b17b7b3f9c4d54b52590e63d7825ede6d3805036
Andy Edwards <jedwards@fastmail.com>
2016-04-05 14:06:50 -0500

    create applyTimestamps

---
+++ meteor/applyTimestamps.js
@@ -0,0 +1,39 @@
+export default function applyTimestamps(collection) {
+  return class TimestampCollection extends collection {
+    insert(document, ...args) {
+      let date = new Date();
+      return super.insert(Object.assign({}, document, {
+        insertedTimestamp: date,
+        updatedTimestamp: date
+      }), ...args);
+    }
+    update(selector, modifier, ...args) {
+      let options = args[0] || {};
+      let date = new Date();
+      modifier = Object.assign({}, modifier, {
+        $set: Object.assign({}, modifier.$set, {
+          updatedTimestamp: date
+        })
+      }, options.upsert ? {
+        $setOnInsert: Object.assign({}, modifier.$setOnInsert, {
+          insertedTimestamp: date 
+        })
+      } : undefined);
+      
+      return super.update(selector, modifier, ...args);
+    }
+    upsert(selector, modifier, ...args) {
+      let date = new Date();
+      modifier = Object.assign({}, modifier, {
+        $set: Object.assign({}, modifier.$set, {
+          updatedTimestamp: date
+        }),
+        $setOnInsert: Object.assign({}, modifier.$setOnInsert, {
+          insertedTimestamp: date 
+        })
+      });
+      
+      return super.upsert(selector, modifier, ...args);
+    }
+  };
+}
