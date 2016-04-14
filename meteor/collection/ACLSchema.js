9bdb8e60c156e42ef7581a6ae26f255c44877603
Andy Edwards <jedwards@fastmail.com>
2016-04-14 10:19:12 -0500

    export single ACLEntry match pattern

--- meteor/collection/ACLSchema.js
+++ meteor/collection/ACLSchema.js
@@ -1,12 +1,12 @@
-export default [
-  Match.OneOf({
-    user: String,
-    permission: String
-  }, {
-    group: String,
-    permission: String
-  }, {
-    role: String,
-    permission: String
-  })
-];
+export const ACLEntry = Match.OneOf({
+  user: String,
+  permission: String
+}, {
+  group: String,
+  permission: String
+}, {
+  role: String,
+  permission: String
+});
+
+export default [ACLEntry];
