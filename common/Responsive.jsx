e24160ce72ca24e869fc78e1d6686816485f947a
Andy Edwards <jedwards@fastmail.com>
2016-04-19 16:33:59 -0500

    create auto mode for SplitTable

---
+++ common/Responsive.jsx
@@ -0,0 +1,66 @@
+/* @flow */
+
+import React, {Component} from 'react';
+import shallowEqual from 'fbjs/lib/shallowEqual';
+import _ from 'lodash';
+
+type Props = {
+  domProps?: string[],
+  computedStyleProps?: string[],
+  children: (state: Object) => ?React.Element,
+  component: string
+};
+
+type DefaultProps = {
+  component: string
+};
+
+type State = Object;
+
+export default class Responsive extends Component<DefaultProps,Props,State> {
+  static defaultProps = {
+    component: 'div'
+  };
+  state: State = {
+    remeasure: this.remeasure
+  }; 
+  mounted: boolean = false;
+  root: ?Object;
+  componentWillMount() {
+    this.mounted = true;
+  }
+  componentDidMount() {
+    this.remeasure();
+    window.addEventListener('resize', this.remeasure);
+  }
+  componentWillReceiveProps(nextProps: Props) {
+    if (!shallowEqual(this.props.domProps, nextProps.domProps) || 
+        !shallowEqual(this.props.computedStyleProps, nextProps.computedStyleProps)) {
+      this.remeasure();
+    }
+  }
+  componentWillUnmount() {
+    this.mounted = false;
+    window.removeEventListener('resize', this.remeasure);
+  }
+  remeasure: Function = _.throttle(() => {
+    const {root} = this;
+    if (this.mounted && root) {
+      let {domProps, computedStyleProps} = this.props;
+      let nextState = {};
+      if (domProps) {
+        domProps.forEach(prop => nextState[prop] = root[prop]);
+      }
+      if (computedStyleProps) {
+        let computedStyle = getComputedStyle(root);
+        computedStyleProps.forEach(prop => nextState[prop] = computedStyle[prop]);
+      }
+      this.setState(nextState);
+    } 
+  }, 500);
+  render(): ?React.Element {
+    let {props: {children}, state} = this;
+    let Comp: any = this.props.component;
+    return <Comp ref={c => this.root= c} children={children(state)}/>;
+  }
+}
--- common/SplitTable.jsx
+++ common/SplitTable.jsx
@@ -1,24 +1,38 @@
 import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
 
+import Responsive from './Responsive.jsx';
+
 /**
  * This is a skin for the skinnable Table (see ./Table.jsx) that splits it up into multiple tables, wrapping the rows
  * just like rows of text in a multi-column article flow.
  */
 export default class SplitTable extends Component {
-  render() {
-    let {className, numParts, children} = this.props;
+  renderParts = (numParts) => {
+    let {className, children} = this.props;
     className = classNames(className, 'mf-SplitTable');
     
     let width = (100 / numParts) + '%';
-
+    
     let tables = [];
     for (let part = 0; part < numParts; part++) {
       tables.push(<PartialTable key={part} part={part} numParts={numParts} children={children}
                                 style={{display: 'inline-block', verticalAlign: 'top', width}}/>);
     }
-
     return <div {...this.props} className={className} children={tables}/>;
+  };
+  
+  render() {
+    let {numParts, minPartWidth} = this.props;
+   
+    if ((numParts === 'auto' || numParts == null) && Number.isFinite(minPartWidth)) {
+      return <Responsive domProps={['offsetWidth']}>
+        {({offsetWidth}) => offsetWidth ? 
+          this.renderParts(Math.max(1, Math.floor(offsetWidth / minPartWidth))) : null}
+      </Responsive>;
+    }
+    
+    return this.renderParts(numParts || 1);
   }
 }
 
