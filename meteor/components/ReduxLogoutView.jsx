3a5e3b894063b641413e9a090d87c06e6cc0154e
Andy Edwards <jedwards@fastmail.com>
2016-04-20 13:15:53 -0500

    redirect to / when user logs out

--- meteor/components/ReduxLogoutView.jsx
+++ meteor/components/ReduxLogoutView.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 
 import Alert from '../../bootstrap/Alert';
@@ -7,10 +7,18 @@ import Spinner from '../../common/Spinner';
 import * as actions from '../actions/userActions';
 
 class ReduxLogoutView extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
   componentWillMount() {
     let {dispatch} = this.props;
     dispatch(actions.setLogoutError(undefined));
-    Meteor.logout(err => dispatch(actions.setLogoutError(err)));
+    Meteor.logout(err => {
+      dispatch(actions.setLogoutError(err));
+      if (!err) {
+        if (this.context.router) setTimeout(() => this.context.router.push('/'), 100); 
+      }
+    });
   }
   render() {
     let {logoutError} = this.props;
