c370a876bae464c2010109484c21626042c2d08b
Andy Edwards <jedwards@fastmail.com>
2016-05-09 12:33:15 -0500

    document pluginRouteSupport

--- react-router/pluginRouteSupport.jsx
+++ react-router/pluginRouteSupport.jsx
@@ -7,6 +7,22 @@ import _ from 'lodash';
 import selectPluginRoutes from '../selectors/selectPluginRoutes';
 import {Store} from '../flowtypes/reduxTypes';
 
+/**
+ * Creates a <Route> decorator that allows you to use the following props on <Route>s:
+ * - getChildRoutesFromStore: like getChildRoutes, but a the given (Redux) store will be passed to it.  Arguments are:
+ *   - location: as in getChildRoutes
+ *   - store: the Redux store
+ *   - cb: the callback to call with an array of <Route>s
+ * - pluginChildRoutesKey: if given, will get any child routes from Redux plugins using selectPluginRoutes.
+ * 
+ * These properties are supported by recursively cloning the decorated <Route> and its descendants and injecting
+ * getChildRoutes props that call getChildRoutesFromStore if given, handle pluginChildRoutesKey if given, and call the
+ * original getChildRoutes if given.  The routes returned from these methods will also be decorated by this
+ * pluginRouteSupport.
+ * 
+ * @param store the Redux store to use.
+ * @returns {decorateTree} a decorator for a <Route>
+ */
 export default function pluginRouteSupport(store: Store): (elem: React.Element) => React.Element {
   return function decorateTree(element: React.Element): React.Element {
     if (element.type === Route) {
