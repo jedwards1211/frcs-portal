6b55f2d9f66827a9d72af3c3a1b62572eee06941
Andy Edwards <jedwards@fastmail.com>
2016-05-10 10:13:37 -0500

    ignore falsey Glyphicon props

--- bootstrap/Glyphicon.jsx
+++ bootstrap/Glyphicon.jsx
@@ -16,7 +16,7 @@ const EXCLUDED_PROPS = [
 export default props => {
   let otherProps = _.pick(props, EXCLUDED_PROPS);
   let className = classNames(props.className, 'glyphicon',
-    _.mapKeys(_.omit(props, EXCLUDED_PROPS), (val, prop) => prop && 'glyphicon-' + _.kebabCase(prop)));
+    _.mapKeys(_.omit(props, EXCLUDED_PROPS), (val, prop) => val ? prop && 'glyphicon-' + _.kebabCase(prop) : ''));
   otherProps.style = propAssign(otherProps.style, {float: props.float});
   return <i {...otherProps} className={className}/>;
 };
