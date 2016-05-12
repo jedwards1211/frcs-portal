cb9154e44408d2ae451ac28bdf269df95f8f1da2
Andy Edwards <jedwards@fastmail.com>
2016-05-12 13:06:28 -0500

    fix karma test conf

--- test/clientUnitTests.js
+++ test/clientUnitTests.js
@@ -1,2 +1,4 @@
+require('../../webpack/lib/core-js-no-number')
+require('regenerator-runtime/runtime')
 var req = require.context('../', true, /__tests__\/(client\/)?unit\/.*\.jsx?$/)
 req.keys().forEach(req)
