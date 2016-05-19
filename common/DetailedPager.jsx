09c555a6dd82d715328a69de16081caadcb3b019
Andy Edwards <jedwards@fastmail.com>
2016-05-18 19:46:52 -0500

    add customized rendering option to DetailedPager

--- common/DetailedPager.jsx
+++ common/DetailedPager.jsx
@@ -7,6 +7,7 @@ import _ from 'lodash'
 import './DetailedPager.sass'
 
 type Props = {
+  children?: (content: any[]) => React.Element,
   className?: string,
   page?: number,
   onPageChange?: (value: number) => any,
@@ -18,7 +19,7 @@ type Props = {
 
 export default class DetailedPager extends Component<void, Props, void> {
   render(): React.Element {
-    let {numItems, page, itemsPerPage, offset} = this.props
+    let {children, numItems, page, itemsPerPage, offset} = this.props
 
     if (!_.isFinite(numItems) || !_.isFinite(page) || !_.isFinite(itemsPerPage) ||
         !_.isFinite(offset)) {
@@ -31,9 +32,16 @@ export default class DetailedPager extends Component<void, Props, void> {
       to = Math.min(from + itemsPerPage - 1, numItems)
     }
     let numPages = Math.ceil(numItems / itemsPerPage)
-    return <span className="mf-detailed-pager">
-      {page != null && <span className="mf-detailed-pager-items">{`Showing ${from}-${to} of ${numItems}`}</span>}
-      {numPages > 1 && <Pager {...this.props} numPages={numPages} />}
-    </span>
+
+    const content = []
+    if (page != null) content.push(
+      <span className="mf-detailed-pager-items">{`Showing ${from}-${to} of ${numItems}`}</span>
+    )
+    if (numPages > 1) content.push(
+      <Pager {...this.props} numPages={numPages} />
+    )
+
+    if (children) return children(content)
+    return <span className="mf-detailed-pager">{content}</span>
   }
 }
