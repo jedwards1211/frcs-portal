8acd6abd88a5830bbd5b8981b156b268e0c7a151
Andy Edwards <jedwards@fastmail.com>
2016-07-14 23:36:38 -0500

    fix multiple issues with ldap server

--- src/server/ldap/ldapConfig.js
+++ src/server/ldap/ldapConfig.js
@@ -9,17 +9,18 @@ function checkMissing(...envvars) {
   })
 }
 
-checkMissing([
-  'LDAP_ADMIN_PASSWORD'
-])
+checkMissing(
+  'LDAP_ADMIN_PASSWORD',
+  'LDAP_ORGANIZATION'
+)
 
 const config = {
   port: process.env.LDAP_PORT || 1389,
   baseDN: process.env.LDAP_BASE || 'dc=example,dc=org',
-  usersOU: process.env.LDAP_USERS_OU || 'ou=users',
-  groupsOU: process.env.LDAP_GROUPS_OU || 'ou=groups',
-  ocgroupsOU: process.env.LDAP_OCGROUPS_OU || 'ou=ocgroups',
-  adminCN: process.env.LDAP_ADMIN_CN,
+  usersOU: process.env.LDAP_USERS_OU || 'users',
+  groupsOU: process.env.LDAP_GROUPS_OU || 'groups',
+  ocgroupsOU: process.env.LDAP_OCGROUPS_OU || 'ocgroups',
+  adminCN: process.env.LDAP_ADMIN_CN || 'admin',
   adminPassword: process.env.LDAP_ADMIN_PASSWORD,
   organization: process.env.LDAP_ORGANIZATION
 }
@@ -31,53 +32,58 @@ config.base = {
     'dcObject',
     'organization'
   ],
+  o: config.organization,
   attributes: {
     // TODO: dc
     o: config.organization
   }
 }
 
-config.adminDN = `${config.adminCN},${config.baseDN}`
+config.adminDN = `cn=${config.adminCN},${config.baseDN}`
 config.admin = {
   dn: config.adminDN,
   objectclass: [
     'simpleSecurityObject',
     'organizationalRole'
   ],
+  cn: config.adminCN,
   attributes: {
     cn: config.adminCN,
     description: 'LDAP administrator'
   }
 }
 
-config.usersDN = `${config.usersOU},${config.baseDN}`
+config.usersDN = `ou=${config.usersOU},${config.baseDN}`
 config.users = {
   dn: config.usersDN,
   objectclass: ['organizationalUnit', 'top'],
+  ou: config.usersOU,
   attributes: {
     ou: config.usersOU
   }
 }
 
-config.groupsDN = `${config.groupsOU},${config.baseDN}`
+config.groupsDN = `ou=${config.groupsOU},${config.baseDN}`
 config.groups = {
   dn: config.groupsDN,
   objectclass: [
     'organizationalUnit',
     'top'
   ],
+  ou: config.groupsOU,
   attributes: {
     ou: config.groupsOU
   }
 }
 
-config.ocgroupsDN = `${config.ocgroupsOU},${config.baseDN}`
+config.ocgroupsDN = `ou=${config.ocgroupsOU},${config.baseDN}`
 config.ocgroups = {
   dn: config.ocgroupsDN,
   objectclass: [
     'organizationalUnit',
     'top'
   ],
+  ou: config.ocgroupsOU,
   attributes: {
     ou: config.ocgroupsOU
   }
