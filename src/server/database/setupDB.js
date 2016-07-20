ec101ea54bf93e1d0c0c292fb3b7eeb6efd12e07
Andy Edwards <jedwards@fastmail.com>
2016-07-19 23:37:24 -0500

    set up groupnames

--- src/server/database/setupDB.js
+++ src/server/database/setupDB.js
@@ -8,8 +8,14 @@ const database = [
   {name: 'users', indices: ['username', 'email']},
   {name: 'groups', indices: ['groupname']},
   {name: 'users_groups', indices: ['user_id', 'group_id']},
+  {name: 'init_user_groups', indices: ['email']},
   {name: 'lanes', indices: ['userId']},
   {name: 'notes', indices: ['laneId']}
+]
+
+const groupnames = [
+  'frcs',
+  'frcs-admin'
 ]
 
 export default async function setupDB(isUpdate = false) {
@@ -19,20 +25,20 @@ export default async function setupDB(isUpdate = false) {
 }
 
 async function reset({db, isUpdate}) {
-  const dbList = await r.dbList()
+  const dbList = await r.dbList().run()
   if (dbList.indexOf(db) === -1) {
     logger.log(`>>Creating Database: ${db}`)
-    await r.dbCreate(db)
+    await r.dbCreate(db).run()
   }
-  const tables = await r.db(db).tableList()
+  const tables = await r.db(db).tableList().run()
   if (!isUpdate) {
     logger.log(`>>Dropping tables in: ${db}`)
-    await Promise.all(tables.map(table => r.db(db).tableDrop(table)))
+    await Promise.all(tables.map(table => r.db(db).tableDrop(table).run()))
   }
   await Promise.all(database.map(table => {
     if (!isUpdate || tables.indexOf(table.name) === -1) {
       logger.log(`>>Creating table ${table.name} in: ${db}`)
-      return r.db(db).tableCreate(table.name)
+      return r.db(db).tableCreate(table.name).run()
     }
     return Promise.resolve(false)
   }))
@@ -40,14 +46,20 @@ async function reset({db, isUpdate}) {
     logger.log(`>>Adding indices on table ${table.name} in: ${db}`)
     return r.db(db).table(table.name).indexList().run()
   }))
-  await Promise.all([...database.map((table, i) => {
+  await Promise.all(database.map((table, i) => {
     const indicies = tableIndicies[i] || []
-    return table.indices.map(index => {
+    return Promise.all(table.indices.map(async index => {
       if (indicies.indexOf(index) === -1) {
-        return r.db(db).table(table.name).indexCreate(index).run()
+        await r.db(db).table(table.name).indexCreate(index).run()
+        return r.db(db).table(table.name).indexWait().run()
       }
       return Promise.resolve(false)
-    })
-  })])
+    }))
+  }))
+  await Promise.all(groupnames.map(groupname => {
+    return r.branch(r.db(db).table('groups').getAll(groupname, {index: 'groupname'}).isEmpty(),
+      r.db(db).table('groups').insert({groupname}),
+      { }).run()
+  }))
   logger.log(`>>${isUpdate ? 'Update' : 'Setup'} complete for: ${db}`)
 }
